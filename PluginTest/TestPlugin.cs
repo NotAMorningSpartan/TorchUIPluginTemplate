60c1dea004a37f5bdba88d7a0b0d62805950ad26
Tyler Kness-Miller <tylerkm75@gmail.com>
2021-05-15 16:03:19 -0600

    Add project files.

---
+++ PluginTest/TestPlugin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using Torch;
+using Torch.API;
+
+namespace PluginTest
+{
+    public class TestPlugin : TorchPluginBase
+    {
+        public static readonly Logger log = LogManager.GetCurrentClassLogger();
+
+        public override void Init(ITorchBase torch)
+        {
+            base.Init(torch);
+            log.Info("Logger test.");
+        }
+    }
+}
